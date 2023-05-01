c3c070c3701c40d25da9a4a737bff78767e73efc
Aleksander Dreszler <olek.d93@gmail.com>
2023-05-01 16:29:53 +0200

    Wykonano zadanie

--- src/main/java/pl/javastart/voting/Vote.java
+++ src/main/java/pl/javastart/voting/Vote.java
@@ -1,12 +1,13 @@
 package pl.javastart.voting;
 
+import static pl.javastart.voting.VoteVariants.*;
+
 // klasa pomocnicza, możesz ją dowolnie zmieniać, albo usunąć
 public class Vote {
-
     private String voter;
-    private Boolean vote; // true - ZA, false - PRZECIW, null - wstrzymanie się
+    private String vote;
 
-    public Vote(String voter, Boolean vote) {
+    public Vote(String voter, String vote) {
         this.voter = voter;
         this.vote = vote;
     }
@@ -15,7 +16,22 @@ public class Vote {
         return voter;
     }
 
-    public Boolean getVote() {
+    public String getVote() {
         return vote;
+    }
+
+    @Override
+    public String toString() {
+        String voteInfo = voter + ": ";
+        switch (vote) {
+            case FAVOUR -> voteInfo += "ZA";
+            case OPPOSE -> voteInfo += "PRZECIW";
+            case ABSTAIN -> voteInfo += "WSTRZYMAŁ SIĘ";
+            default -> {
+                //
+            }
+        }
+
+        return voteInfo;
     }
 }
--- src/main/java/pl/javastart/voting/Voting.java
+++ src/main/java/pl/javastart/voting/Voting.java
@@ -4,21 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import static pl.javastart.voting.VoteVariants.*;
+
 public class Voting {
 
     public static void main(String[] args) {
         List<String> voters = new ArrayList<>();
 
         // możesz dowolnie dodawać / usuwać dane testowe
-        voters.add("Jan Kowalski");
-        voters.add("Zigniew Siobro");
-        voters.add("Zbyszek Stonoga");
+        voters.add("Janusz Kowalski");
+        voters.add("Zbigniew Stonoga");
+        voters.add("Janusz Korwin-Mikke");
 
         Voting voting = new Voting();
 
         VotingResult votingResult = voting.executeVoting(voters, new Scanner(System.in));
         votingResult.printResults();
-        votingResult.printVoteForVoter("Zigniew Siobro");
+        votingResult.printVoteForVoter("Janusz Kowalski");
     }
 
     /**
@@ -27,8 +29,35 @@ public class Voting {
      * Metoda powinna pobrać głos dla każdego przekazanego głosującego i zapisać wyniki głosowania do VotingResult
      */
     VotingResult executeVoting(List<String> voters, Scanner scanner) {
+        List<Vote> votes = new ArrayList<>();
 
-        return null; // to możesz (a nawet powinieneś/powinnaś) zmienić :)
+        for (String voter : voters) {
+            boolean voteValid = false;
+            Vote vote = null;
+            while (!voteValid) {
+                System.out.printf("Jak głosuje %s? (z - za, p - przeciw, w - wstrzymanie się)\n", voter);
+                switch (scanner.nextLine()) {
+                    case FAVOUR -> {
+                        vote = new Vote(voter, FAVOUR);
+                        voteValid = true;
+                    }
+                    case OPPOSE -> {
+                        vote = new Vote(voter, OPPOSE);
+                        voteValid = true;
+                    }
+                    case ABSTAIN -> {
+                        vote = new Vote(voter, ABSTAIN);
+                        voteValid = true;
+                    }
+                    default -> System.out.println("Nieprawidłowa opcja");
+                }
+            }
+            votes.add(vote);
+        }
+
+        VotingResult votingResult = new VotingResult(votes);
+
+        return votingResult;
     }
 
 }
